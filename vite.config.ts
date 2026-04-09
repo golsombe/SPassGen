dcfd4b2dd571e4151d7e8df9d18fc22fb4f0a07d
Robert Hall <golsombe@gmail.com>
2026-04-08 20:11:22 -0400

    Trying a fresh deploy.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dns from 'node:dns'
 
-dns.setDefaultResultOrder('verbatim')
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/SPassGen/', // Must match your GitHub repo name
   plugins: [react()],
-  server: {
-  port: 8080,
-  host: '192.168.86.179'
-}}) 
+  base: '/SPassGen/',
+})
+
